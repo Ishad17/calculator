faf94de73e556245843cb68d876d6ac391756f9e
ishad ramzan <ishadh99@gmail.com>
2025-08-03 14:51:04 +0530

    function building

--- script.js
+++ script.js
@@ -1,47 +1,159 @@
 console.log( "hello world!")
 
-        const buttons = document.querySelectorAll(".btn");
-        const container = document.getElementById("display-container");
+function operate(num1, operator, num2 ) {
+    
+    if(operator == '+' ) {
+        value = (num1 + num2);
+        return value
+    }else if(operator == '-'){
+        value = num1 - num2;
+        return value;
+    }else if(operator == '%') {
+        value = num1 % num2;
+        return value;
+    }else if(operator == '*') {
+        value = num1 * num2;
+        return value;
+    } 
+    if(operator == '/' && num2 != 0) {
+        value = num1 / num2;
+        return value;
+    }else if (operator == '/' && num2 === 0){
+            return 'ERROR'
+    }
+}
 
+console.log(operate(80,'*',50));
+
+
+
+        const buttons = document.querySelectorAll(".btn");
+        const buttonsOperator = document.querySelectorAll(".opt");
+        const container = document.getElementById("display-container");
+        const clearButton = document.getElementById("btn-clear");
 
         buttons.forEach(button => 
            
-            button.addEventListener("click", function() {   
+        button.addEventListener("click", function() {   
 
-                   const newDiv = document.createElement("div");
+        const operator = document.querySelectorAll(".display-opt");
+
+            if( this.classList.contains("opt") ){
+                console.log("your!")
+                               
+                operator.textContent = button.textContent;
+                return;
+
+            }
+
+                //    container.textContent = button.textContent;
+                   container.textContent += button.textContent; 
+                   console.log(typeof button.textContent);
+                   let value1 = container.textContent;   
                    
-                    newDiv.id = "display";
-                    newDiv.style.width = "10px";
-                    newDiv.style.height = "10px";
-                    newDiv.style.padding = "20px";
-                    newDiv.style.color = "white";
-                    newDiv.style.borderRadius = "5px";
-                    newDiv.style.margin = "5px";
-                    newDiv.style.backgroundColor = "#035c54ff";
-                    newDiv.textContent = button.textContent;
-                    container.appendChild(newDiv);
+                   console.log(container.textContent);
+                  
 
+                    // let num1 = button.textContent; 
 
-                // const outputDiv = document.getElementById("display");
-                // const text = button.textContent; // Get the text inside the button
-                // outputDiv.textContent = text;
+                    // const displayValue = document.getElementById("#display-container")
+                        
+                    // button.style.backgroundColor = "rgba(1, 60, 255, 1)";
+                    // setTimeout(() => {
+                    //     button.style.backgroundColor = ""; // resets to default or previous style
+                    // }, 200);
+
             })
+            
         );
+        
+
+        clearButton.addEventListener("click" , function() {
+        document.getElementById("display-container").innerHTML = "";
+        console.clear();
+                      
+            this.style.backgroundColor = 'rgba(184, 0, 0, 1)'
+             setTimeout(() => {
+                        this.style.backgroundColor = ""; // resets to default or previous style
+                    }, 200);
+                return 2;
+        });
 
 
-function add(a, b) {
-    return a + b;
-}
-function subtract(a, b) {
-    return a - b;
-}
-function multiply(a, b) {
-    return a * b;
-}
-function divide(a, b) {
-    if (b === 0) {
-    return "ERROR"
-    }
-    return a / b;
-}
+let operator;
+let num2;
 
+
+
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+// function multiply(a, b) {
+//     return a * b;
+// }
+// function divide(a, b) {
+//     if (b === 0) {
+//     return "ERROR"
+//     }
+//     return a / b;
+// }
+
+
+
+                    // if(operator == '+' ) {
+                    //     value = (num1 + num2);
+                    //     return value
+                    // }else if(operator == '-'){
+                    //     value = num1 - num2;
+                    //     return value;
+                    // }else if(operator == '%') {
+                    //     value = num1 % num2;
+                    //     return value;
+                    // }else if(operator == '*') {
+                    //     value = num1 * num2;
+                    //     return value;
+                    // } 
+                    // if(operator == '/' && num2 != 0) {
+                    //     value = num1 / num2;
+                    //     return value;
+                    // }else if (operator == '/' && num2 === 0){
+                    //         return 'ERROR'
+                    // }
+
+
+
+
+                    
+        // const buttonsNum = document.querySelectorAll(".num");
+        // const buttonsOperator = document.querySelectorAll(".opt");
+        // const container = document.getElementById("display-container");
+        // const clearButton = document.getElementById("btn-clear");
+
+        // buttonsNum.forEach(buttonNum =>
+
+        // buttonNum.addEventListener("click", function() {
+            
+
+        //         //    container.textContent = button.textContent;
+        //            container.textContent += buttonNum.textContent; 
+        //            console.log(typeof buttonNum.textContent);
+        //            let value1 = container.textContent;   
+        //         //    return value1;
+        //            console.log(container.textContent);
+
+                   
+                  
+
+        //             // let num1 = button.textContent; 
+
+        //             // const displayValue = document.getElementById("#display-container")
+                        
+        //             // button.style.backgroundColor = "rgba(1, 60, 255, 1)";
+        //             // setTimeout(() => {
+        //             //     button.style.backgroundColor = ""; // resets to default or previous style
+        //             // }, 200);
+
+        //     })
+            
+        // );
