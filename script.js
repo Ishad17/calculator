efd43162385a9f158dae5b6177739122da8c318d
ishad17 <ishadh99@gmail.com>
2025-08-04 18:49:03 +0530

    fixing

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ console.log( "hello world!")
 function operate(num1, operator, num2 ) {
     
     if(operator == '+' ) {
-        value = (num1 + num2);
-        return value
+        num1 = (num1 + num2);
+        return num1
+        
     }else if(operator == '-'){
         value = num1 - num2;
         return value;
@@ -32,62 +33,76 @@ console.log(operate(80,'*',50));
         const containerOpt =  document.getElementById("display-opt");
         const clearButton = document.getElementById("btn-clear");
 
-
-                let value1;
-                let value2;
-                let operator;
+        let value1;
+        let value2;
+        let operator;
+        let total;
 
         buttons.forEach(button => 
            
-            button.addEventListener("click", function() {   
+            button.addEventListener("click", function() {
 
-                // console.log(operator);
-
-                if(operator){
-                        if(!value2){
-                            document.getElementById("display-num").innerHTML = "";
-                        }
-
-                        containerNum.textContent += button.textContent; 
-                        value2 = containerNum.textContent + ': Value2';   
-                        value2 = parseInt(value2);
-                        }
-
-
-                    if(this.classList.contains("opt") ){
+                        if((this.classList.contains("opt"))){
+                            if(!value2){
+                                document.getElementById("display-num").innerHTML = "";
+                            }
+                        // console.log("no value2?")
+                        //     containerNum.textContent += button.textContent; 
+                        //     total = containerNum.textContent;
+                        //     total = parseInt(total);
+                        //     total = total + value2;
+                        //     containerNum.innerHTML = `${operate(value1,operator,value2)}`;
+                        // }
+                        console.log(operator);
                         // document.getElementById("display-num").innerHTML = "";
-                            operator = containerOpt.textContent = button.textContent;
-                            // console.log(operator);
-                        //     console.log(typeof operator);
-
-                        // console.log(value1)
-
-                    }else if(this.classList.contains("num") && !(value2) ){
                         containerNum.textContent += button.textContent; 
-                        value1 = containerNum.textContent + ': Value1';   
+                        value2 = containerNum.textContent;
+                        value2 = parseInt(value2);
+                        console.log('value2 : ' + value2);
+                        return;
+                        }
+
+                    if(this.classList.contains("opt")  ){
+                        document.getElementById("display-num").innerHTML = "";
+                        // total = operate(value1,operator,value2);
+                        operator = containerOpt.textContent = button.textContent;
+                        // console.log(typeof operator);
+                        console.log('value1 : ' + value1);
+                        console.log(operator);
+                        // console.log(total)
+                        // containerNum.innerHTML = `${total}`;
+
+                    }else if(this.classList.contains("num")){
+                        
+                        containerNum.textContent += button.textContent; 
+                        value1 = containerNum.textContent;                                        /// value 1 
                         value1 = parseInt(value1);
                         console.log('value1 : ' + value1);
-                    
+
                     }else if(this.classList.contains("clear") ){
-                            document.getElementById("display-num").innerHTML = "";
-                            document.getElementById("display-opt").innerHTML = "";
-                              value1 = '';
-                                value2 = '';
-                                operator = '';                           
+                            containerNum.innerHTML = "";
+                            containerOpt.innerHTML = "";
+                            value1 = '';
+                            value2 = '';
+                            operator = '';                           
                             console.clear();        
                     }
-
                          if(this.classList.contains("equal")){
-                            containerOpt.textContent = button.textContent;
+
                             console.log("val 1 : " + value1);
                             console.log("val 2 : " + value2);
-                            console.log(operate(value1,operator,value2));
+                            console.log(operator);
 
-                            document.getElementById("display-num").innerHTML = `${operate(value1,operator,value2)}`;
-                            return;
+                            containerNum.innerHTML = `${operate(value1,operator,value2)}`;
+                            // containerOpt.textContent = button.textContent;
+                            // console.log("val 1 : " + value1);
+                            // console.log("val 2 : " + value2);
+                            // console.log( value1 + value1)
+                            console.log(operate(value1,operator,value2));
+                            
+                            // console.log('total : ' + total);
+                           
                          }
-                    
-                
                 })
         );
 
